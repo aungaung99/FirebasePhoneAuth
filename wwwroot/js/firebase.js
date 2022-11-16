47c51c2890dbf8d988bbec96d3818ed97dd8e3a9
Aung Naing Oo <aungaung20.ab@gmail.com>
2022-11-16 23:30:25 +0630

    update phone auth completed

--- wwwroot/js/firebase.js
+++ wwwroot/js/firebase.js
@@ -1,6 +1,16 @@
 ﻿// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-import { getAuth, signInWithPhoneNumber } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+import {
+    getAuth,
+    signInWithPhoneNumber,
+    RecaptchaVerifier,
+    PhoneAuthProvider,
+    signInWithCredential,
+    onAuthStateChanged,
+    getIdToken,
+    PhoneAuthCredential,
+    applyActionCode
+} from "../firebase/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,24 +26,70 @@ const firebaseConfig = {
     measurementId: "G-JVGDM7TVWE"
 };
 
+// Element
+const signInWithPhoneButton = document.getElementById('signInWithPhoneButton');
+const getCodeButton = document.getElementById('getCode');
+const phoneNumberText = document.getElementById('phoneNumber');
+const verifyCodeTextBox = document.getElementById('verifyCode');
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-auth.languageCode = 'mm';
 
-const phoneNumber = "+959885171061s";
-const appVerifier = window.recaptchaVerifier;
-appVerifier.verfiy();
-signInWithPhoneNumber(auth, phoneNumber, appVerifier)
-    .then((confirmationResult) => {
-        console.log('Hello');
-        // SMS sent. Prompt user to type the code from the message, then sign the
-        // user in with confirmationResult.confirm(code).
-        //window.confirmationResult = confirmationResult;
-        //console.log(confirmationResult);
-        // ...
-    }).catch((error) => {
-        console.error('Error', error.message);
-        // Error; SMS not sent
-        // ...
+let confirmResult;
+
+window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {}, auth);
+recaptchaVerifier.render().then(widgetId => {
+    window.recaptchaWidgetId = widgetId;
+});
+
+const sendVerificationCode = () => {
+    const phoneNumber = phoneNumberText.value;
+    const appVerifier = window.recaptchaVerifier;
+
+    signInWithPhoneNumber(auth, phoneNumber, appVerifier).then(confirmationResult => {
+        const sentCodeId = confirmationResult.verificationId;
+        confirmResult = confirmationResult;
+        
+        window.confirmationResult = confirmationResult;
+        console.log(confirmationResult);
+        signInWithPhoneButton.addEventListener('click', () => signInWithPhone(sentCodeId));
+    }).catch(error => {
+        console.log(error);
     });
+}
+
+getCodeButton.addEventListener('click', () => sendVerificationCode());
+
+const signInWithPhone = sentCodeId => {
+    const code = verifyCodeTextBox.value;
+    confirmResult.confirm(code).then((result) => {
+        const user = result.user;
+        console.log(result);
+    })
+
+    //const code = verifyCodeTextBox.value;
+    //console.log({ sentCodeId, code });
+    //const credential = PhoneAuthProvider.credential(sentCodeId, code);
+    //console.log(credential);
+
+    //onAuthStateChanged(auth, (user) => {
+    //    if (user) {
+    //        console.log(user);
+    //    }
+    //    else {
+    //        console.log(user);
+    //        console.log('User signed out');
+    //    }
+    //});
+
+    //PhoneAuthCredential(credential).then(() => { }).catch(error => {
+    //    console.log(error);
+    //});
+    //signInWithCredential(credential)
+    //    .then(() => {
+    //        /* window.location.assign('./privacy');*/
+    //    })
+    //    .catch(error => {
+    //        console.log(error);
+    //    });
+};
